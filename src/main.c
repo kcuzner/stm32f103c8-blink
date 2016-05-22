e369ba0f189010235ebd7a8245c101bfc19dcaa6
Kevin Cuzner <kevin@kevincuzner.com>
2016-05-21 21:27:10 -0600

    Added some comments

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 /**
- * led-watch
+ * STM32F103C8 Blink Demonstration
  *
  * Kevin Cuzner
  */
@@ -8,13 +8,18 @@
 
 int main(void)
 {
+    //Step 1: Enable the clock to PORT B
     RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;
+
+    //Step 2: Change PB0's mode to 0x3 (output) and cfg to 0x0 (push-pull)
     GPIOB->CRL = GPIO_CRL_MODE0_0 | GPIO_CRL_MODE0_1;
 
     while (1)
     {
+        //Step 3: Set PB0 high
         GPIOB->BSRR = GPIO_BSRR_BS0;
         for (uint16_t i = 0; i != 0xffff; i++) { }
+        //Step 4: Reset PB0 low
         GPIOB->BSRR = GPIO_BSRR_BR0;
         for (uint16_t i = 0; i != 0xffff; i++) { }
     }
